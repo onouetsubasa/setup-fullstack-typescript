449841fbb89a16447ab90847e2110ec45db08e99
onouetsubasa <onouetsubasa@gmail.com>
2026-03-15 11:33:40 +0900

    feat(eslint-config): next.js (Next.js 向けルール) を追加

---
+++ packages/eslint-config/next.js
@@ -0,0 +1,17 @@
+// packages/eslint-config/next.js
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import baseConfig from "./base.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...baseConfig,
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
